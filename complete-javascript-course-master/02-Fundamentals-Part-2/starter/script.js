afea2070861729ef7437f1cc680928cbb1bafc0f
Bolarg9764 <bolarg9764@gmail.com>
2021-03-14 07:08:13 -0400

    update 03.14.2021
    some updates made
    intro to objects, start the use of this.method, functions as value inside objects
    let object = {
    this value: function(){ expression}

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -18,7 +18,7 @@ return 2037 - birthday;
     return 2037-birthday;
 } */
 
-const calcAge = function (birthday) {
+/* const calcAge = function (birthday) {
     return 2037 - birthday;
 }
 
@@ -38,7 +38,7 @@ return;}
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Jonas'));
-
+ */
 /* const age2 = calcAge2(1991);
 console.log(age1, age2); */
 
@@ -77,19 +77,19 @@ console.log(fruitProcessor(2, 3)); */
         ====================================== */
 
 
-const calcAverage = (a, b, c) => (a + b + c) / 3;
+/* const calcAverage = (a, b, c) => (a + b + c) / 3;
 // Test 1
 let scoreDolphins = calcAverage(44, 23, 71);
 let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
-
+ */
 
         /*======================================
         second function combine with
         if statement to check if statement one
         is at least double than statement one
         */
-const checkWinner = function (avgDolphins, avgKoalas) {
+/* const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
     console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas >= 2 * avgDolphins) {
@@ -106,7 +106,7 @@ checkWinner(576, 111);
 scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
-checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas); */
 
 
 /* ====================
@@ -117,7 +117,7 @@ the begining of arrays = new array[]
 //Add Elements//
 //push()
 //unshift()
-const morefriends = ['michael2', 'steven2', 'peter2']
+/* const morefriends = ['michael2', 'steven2', 'peter2']
 const friends = ['michael', 'steven', 'peter']
 console.log(friends);
 console.log(friends.length);
@@ -156,27 +156,91 @@ console.log(friends.includes('23')); //Reads it as string 23 and not as a number
 if(friends.includes('steven')) {
 
     console.log(`you have a friend called steven`);
-}
+} */
 
 //Coding challenge #2
 
-let bill = 50;
-let tip = 0;
+// let bill = 50;
+// let tip = 0;
 
-let calctip = function (bill) {
+let calctip = function(bill) {
     if(bill > 300) {
-        let tip = bill * 0.20;
-        console.log(`the tip will be ${tip}`);
-    }else if(bill >= 50 || bill <= 300) { 
-        let tip = bill * 0.15;
-        console.log(`the tip will be ${tip}`);
-        //console.log(tip);
+        return bill * 0.20;
+          }else if(bill >= 50 || bill <= 300) { 
+            return bill * 0.15;
+            }
+    }
+    
+    let bill = [125, 555, 44];
+    let tip = [calctip(bill[0]), calctip(bill[1]), calctip(bill[2])];
+    let total = [bill[0] + tip[0], bill[1] + tip[1], bill[2] + tip[2]];
+    
+      for (let index = 0; index < total.length; index++) {
+        console.log(`the bill is $${bill[index]} + tip $${tip[index]} for a total of $${total[index]}`);
         }
-}
+      console.log(bill, tip, total);
+    
 
-calctip(400); // output the tip will be 80.00
-calctip(50); // output the tip will be 7.5
-calctip(300); // output the tip will be 45.00
+      /*const calcTip = function (bill) {
+        return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+      }
+      // const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+      
+       const bills = [125, 555, 44];
+      const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+      const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+      
+      console.log(bills, tips, totals); */
 
+            /*========================
+              Introduction to Objects 
+            March 14th 2021 04.48am 
+            =========================*/
+
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'], //array value
+  hasdriverlicense: false, //boolean value
+  /* calcAge: function() {
+    return 2037 - this.birthYear; //function value
+  } */
+  calcAge: function() {
+this.age = 2037 - this.birthYear;
+return this.age;  //using `this` keyword to access a value inside same array
+  },
+
+  getSummary: function() {
+    return `${this.firstName} is a ${this.calcAge()} years old ${this.job} and he ${this.hasdriverlicense ? " has a" : "does not have"} a driver license`
+  },
+};
+
+//another way to solve but with if statements
+
+/* if(jonas.hasdriverlicense == true) {
+  console.log(`${jonas.firstName} is ${jonas.lastName} ${jonas.job}, and has a driver license`);
+  } else{
+    console.log(`${jonas.firstName} is ${jonas.lastName} ${jonas.job}, and does not have a driver license`);
+  } */
+
+jonas.location = 'portugal';
+jonas['twitter']='@jonasschedtman';
+console.log(jonas.lastName, jonas.age);
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+console.log(jonas.calcAge()); //output 2037 - 1991
+
+
+console.log(jonas.getSummary());
 
 
